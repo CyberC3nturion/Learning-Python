c5d084c13678190434f6d4b901f99cf781c4cbeb
ddip214 <ddiponzio@gmail.com>
2020-04-24 10:10:40 -0400

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#First exercise in LPTHW3 pg.30
+
+print("Hello World!")
+print("Hello again")
+print("I like typeing this")
+print("This is fun")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this. ')
